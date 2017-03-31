595eb0dda8d769ca82e867681507e3e7fe745571
Grey Patterson <greypatterson@gmail.com>
2017-03-31 12:57:11 -0700

    Try different variables

--- NeuralNet.playground/Contents.swift
+++ NeuralNet.playground/Contents.swift
@@ -4,8 +4,8 @@ import PlaygroundSupport
 struct config{
     static let layerInfo: [Int] = [8, 2] // Default layer structure; [Int], where each value is the number of neurons in the layer. First layer will be InputNeurons, the rest will be Sigmoids
     static let defaultInput: Double = 0.0 // Default input for the InputNeurons
-    static let defaultStepSize: Double = 0.25 // Default step size for training
-    static let stepSizeChange: Double = 0.8 // Multiplier by which to change the step size after every training iteration
+    static let defaultStepSize: Double = 0.2 // Default step size for training
+    static let stepSizeChange: Double = 0.875 // Multiplier by which to change the step size after every training iteration
     static let trainingIterations: Int = 15 // Number of training iterations to run
 }
 
